a0b129b6b573c41954686b1897df670d37c38207
phenphak <48151529+phenphak@users.noreply.github.com>
2021-03-17 14:14:31 +0700

    lab13

---
+++ lab13/Main.java
@@ -0,0 +1,10 @@
+package com.cmd.lab13;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Employee emp = new Employee("Dang", "Oho", 30000, "Progrmmer");
+		System.out.println(emp.checkPosition());
+
+	}
+}
---
+++ lab13/Programmer.java
@@ -0,0 +1,23 @@
+package com.cmd.lab13;
+
+public class Programmer extends Employee {
+	
+
+	
+	
+	public Programmer(String firstnameInput, String lastnameInput, int salaryInput, String positionInput) {
+		super(firstnameInput, lastnameInput, salaryInput, positionInput);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void createWebsite(String template, String titleName) {
+		
+	}
+	
+	public void installWindows(String version, String productKey) {
+		
+	}
+
+
+
+}
